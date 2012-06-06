3000b1af9355b08fcd2e3dded250faadbb821acd
Matthias Kestenholz <mk@spinlock.ch>
2012-06-06 11:34:16 +0200

    Move feincms.utils.html.cleanse into its own package

---
+++ README.rst
@@ -0,0 +1,15 @@
+===============
+feincms-cleanse
+===============
+
+This is the FeinCMS HTML cleansing function which lived under the name of
+:py:func:`feincms.utils.html.cleanse.cleanse_html`, and has been moved to
+its own repository now to be usable in other projects, and to not set a
+precedent for a particular HTML cleaning technique in FeinCMS anymore.
+
+Usage is simple::
+
+    from feincms_cleanse import cleanse_html
+
+    >>> cleanse_html(u'<span style="font-weight:bold">some text</span>')
+    u'<strong>some text</strong>'
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import os
+import setuplib
+
+packages, package_data = setuplib.find_packages('feincms_cleanse')
+
+setup(name='feincms-cleanse',
+    version=__import__('feincms_cleanse').__version__,
+    description='Default HTML cleansing in FeinCMS',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    author='Matthias Kestenholz',
+    author_email='mk@feinheit.ch',
+    url='http://github.com/feincms/feincms-cleanse/',
+    license='BSD License',
+    platforms=['OS Independent'],
+    packages=packages,
+    package_data=package_data,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+    ],
+)
