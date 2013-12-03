b0c6d002019ae11072c3dd90a21c95cad4f7fa6b
Matthias Kestenholz <mk@spinlock.ch>
2013-12-03 19:53:35 +0100

    Reindent the testcases

--- feincms_cleanse/tests.py
+++ feincms_cleanse/tests.py
@@ -9,7 +9,11 @@ class CleanseTestCase(TestCase):
         for before, after in entries:
             after = before if after is None else after
             result = klass().cleanse(before)
-            self.assertEqual(result, after, u"Cleaning '%s', expected '%s' but got '%s'" % (before, after, result))
+            self.assertEqual(
+                result,
+                after,
+                u"Cleaning '%s', expected '%s' but got '%s'" % (
+                    before, after, result))
 
     def test_01_cleanse(self):
         entries = [
@@ -28,50 +32,50 @@ class CleanseTestCase(TestCase):
 
     def test_02_a_tag(self):
         entries = (
-                    ('<a href="/foo">foo</a>', None),
-                    ('<a href="/foo" target="some" name="bar" title="baz" cookies="yesplease">foo</a>', '<a href="/foo" target="some" name="bar" title="baz">foo</a>'),
-                    ('<a href="http://somewhere.else">foo</a>', None),
-                    ('<a href="https://somewhere.else">foo</a>', None),
-                    ('<a href="javascript:alert()">foo</a>', '<a href="">foo</a>'),
-                    ('<a href="javascript%3Aalert()">foo</a>', '<a href="">foo</a>'),
-                  )
+            ('<a href="/foo">foo</a>', None),
+            ('<a href="/foo" target="some" name="bar" title="baz" cookies="yesplease">foo</a>', '<a href="/foo" target="some" name="bar" title="baz">foo</a>'),
+            ('<a href="http://somewhere.else">foo</a>', None),
+            ('<a href="https://somewhere.else">foo</a>', None),
+            ('<a href="javascript:alert()">foo</a>', '<a href="">foo</a>'),
+            ('<a href="javascript%3Aalert()">foo</a>', '<a href="">foo</a>'),
+        )
 
         self.run_tests(entries)
 
     def test_03_merge(self):
         entries = (
-                   ('<h2>foo</h2><h2>bar</h2>', '<h2>foo bar</h2>'),
-                   ('<h2>foo  </h2>   <h2>   bar</h2>', '<h2>foo bar</h2>'),
-                  )
+            ('<h2>foo</h2><h2>bar</h2>', '<h2>foo bar</h2>'),
+            ('<h2>foo  </h2>   <h2>   bar</h2>', '<h2>foo bar</h2>'),
+        )
 
         self.run_tests(entries)
 
     def test_04_p_in_li(self):
         entries = (
-                   ('<li><p>foo</p></li>', '<li> foo </li>'),
-                   ('<li>&nbsp;<p>foo</p> &#160; </li>', '<li>  foo    </li>'),
-                   ('<li>foo<p>bar<strong>xx</strong>rab</p><strong>baz</strong>a<p>b</p>c</li>', '<li>foo bar <strong>xx</strong>rab<strong>baz</strong>a b c</li>'),
-                  )
+            ('<li><p>foo</p></li>', '<li> foo </li>'),
+            ('<li>&nbsp;<p>foo</p> &#160; </li>', '<li>  foo    </li>'),
+            ('<li>foo<p>bar<strong>xx</strong>rab</p><strong>baz</strong>a<p>b</p>c</li>', '<li>foo bar <strong>xx</strong>rab<strong>baz</strong>a b c</li>'),
+        )
 
         self.run_tests(entries)
 
     def test_05_p_in_p(self):
         entries = (
-                   ('<p><p>foo</p></p>', '<p>foo</p>'),
-                   (u'<p><p><p>&nbsp;</p> </p><p><br /></p></p>', u''),
-                   # This is actually correct as the second <p> implicitely
-                   # closes the first paragraph, and the trailing </p> is
-                   # deleted because it has no matching opening <p>
-                   ('<p>foo<p>bar</p>baz</p>', '<p>foo</p><p>bar</p>baz'),
-                  )
+            ('<p><p>foo</p></p>', '<p>foo</p>'),
+            (u'<p><p><p>&nbsp;</p> </p><p><br /></p></p>', u''),
+            # This is actually correct as the second <p> implicitely
+            # closes the first paragraph, and the trailing </p> is
+            # deleted because it has no matching opening <p>
+            ('<p>foo<p>bar</p>baz</p>', '<p>foo</p><p>bar</p>baz'),
+        )
 
         self.run_tests(entries)
 
     def test_06_whitelist(self):
         entries = (
-                   (u'<script src="http://abc">foo</script>', u''),
-                   (u'<script type="text/javascript">foo</script>', u''),
-                  )
+            (u'<script src="http://abc">foo</script>', u''),
+            (u'<script type="text/javascript">foo</script>', u''),
+        )
 
         self.run_tests(entries)
 
@@ -81,16 +85,16 @@ class CleanseTestCase(TestCase):
             allowed_tags = { 'h1': (), 'h2': () }
 
         entries = (
-                   ('<h1>foo</h1>', None),
-                   ('<h1>foo</h1><h2>bar</h2><h3>baz</h3>', '<h1>foo</h1><h2>bar</h2>baz'),
-                  )
+            ('<h1>foo</h1>', None),
+            ('<h1>foo</h1><h2>bar</h2><h3>baz</h3>', '<h1>foo</h1><h2>bar</h2>baz'),
+        )
 
         self.run_tests(entries, klass=MyCleanse)
 
     def test_08_li_with_marker(self):
         entries = (
-                   ('<li> - foo</li>', '<li>foo</li>'),
-                   ('<li>* foo</li>', '<li>foo</li>'),
-                  )
+            ('<li> - foo</li>', '<li>foo</li>'),
+            ('<li>* foo</li>', '<li>foo</li>'),
+        )
 
         self.run_tests(entries)
