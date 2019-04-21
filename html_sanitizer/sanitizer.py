df9088d0de21278c2b89d4158dca942e1152e421
Matthias Kestenholz <mk@feinheit.ch>
2019-04-21 10:22:19 +0200

    Add a comment to the safe_attrs_only=False line

--- html_sanitizer/sanitizer.py
+++ html_sanitizer/sanitizer.py
@@ -338,7 +338,7 @@ class Sanitizer(object):
         lxml.html.clean.Cleaner(
             allow_tags=self.tags,
             remove_unknown_tags=False,
-            safe_attrs_only=False,
+            safe_attrs_only=False,  # Our attributes allowlist is sufficient.
             add_nofollow=self.add_nofollow,
             forms=False,
         )(doc)
